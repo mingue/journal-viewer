8ba5ac1e76e9c32ae673563a5d939ce85335dae9
Victor Mingueza <victormingueza@gmail.com>
2023-03-28 23:00:48 +1300

    use dynamic linking for systemd

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("cargo:rustc-link-lib=systemd");
+    println!("cargo:rustc-link-lib=dylib=systemd");
     println!("cargo:rustc-link-search=native=/usr/lib/");
     tauri_build::build()
 }
