a9c7133e492bb2db766cb4e85fc31645a4e552c6
Victor Mingueza <victormingueza@gmail.com>
2023-03-29 10:16:08 +1300

    fix linking

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,6 +3,5 @@
 set -e
 
 sudo ln -s /lib/x86_64-linux-gnu/libsystemd.so.0 /lib/x86_64-linux-gnu/libsystemd.so
-ld -lsystemd --verbose
-# npm install
-# npm run tauri build
+npm install
+npm run tauri build
