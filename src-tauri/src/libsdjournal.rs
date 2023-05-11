e831aceed5f1e577664c1f732ae66e384e575abc
Victor Mingueza <victormingueza@gmail.com>
2023-05-11 12:08:15 +1200

    Switch to sd_journal_enumerate_data for record enumeration

--- src-tauri/src/libsdjournal.rs
+++ src-tauri/src/libsdjournal.rs
@@ -239,19 +239,22 @@ pub fn sd_journal_seek_realtime_usec(
     Ok(())
 }
 
-pub fn sd_journal_enumerate_available_data(
+pub fn sd_journal_enumerate_data(
     sd_journal: *mut c_void,
 ) -> Result<Option<(String, String)>, JournalError> {
+    // TODO: Refactor so that it returns the whole record not field by field
+    // TODO: Enrich JournalError to avoid using Result<Option>
     let mut data: *mut c_void = std::ptr::null_mut();
     let mut length: size_t = 0;
     let ret: libc::c_int;
 
     unsafe {
-        ret = libsdjournal_bindings::sd_journal_enumerate_available_data(
-            sd_journal,
-            &mut data,
-            &mut length,
-        );
+        ret = libsdjournal_bindings::sd_journal_enumerate_data(sd_journal, &mut data, &mut length);
+    }
+
+    // Skip field in this situation
+    if ret == -libc::E2BIG || ret == -libc::ENOBUFS || ret == -libc::EPROTONOSUPPORT {
+        return Ok(Some(("".to_owned(), "".to_owned())));
     }
 
     if ret < 0 {
@@ -259,7 +262,7 @@ pub fn sd_journal_enumerate_available_data(
     }
 
     if ret == 0 {
-        return Ok(None)
+        return Ok(None);
     }
 
     let result = unsafe {
@@ -279,3 +282,46 @@ pub fn sd_journal_enumerate_available_data(
 
     Ok(result.unwrap())
 }
+
+// pub fn sd_journal_enumerate_available_data(
+//     sd_journal: *mut c_void,
+// ) -> Result<Option<(String, String)>, JournalError> {
+//     // TODO: Refactor so that it returns the whole record not field by field
+//     // TODO: Enrich JournalError to avoid using Result<Option>
+//     let mut data: *mut c_void = std::ptr::null_mut();
+//     let mut length: size_t = 0;
+//     let ret: libc::c_int;
+
+//     unsafe {
+//         ret = libsdjournal_bindings::sd_journal_enumerate_available_data(
+//             sd_journal,
+//             &mut data,
+//             &mut length,
+//         );
+//     }
+
+//     if ret < 0 {
+//         return Err(JournalError(ret));
+//     }
+
+//     if ret == 0 {
+//         return Ok(None);
+//     }
+
+//     let result = unsafe {
+//         match CStr::from_ptr(data as *mut c_char).to_str() {
+//             Ok(s) => {
+//                 let s = String::from(s);
+//                 let values = s.split_once('=').unwrap();
+//                 return Ok(Some((values.0.to_owned(), values.1.to_owned())));
+//             }
+//             Err(_) => Err(-1),
+//         }
+//     };
+
+//     if let Err(e) = result {
+//         return Err(JournalError(e));
+//     }
+
+//     Ok(result.unwrap())
+// }
